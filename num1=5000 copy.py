fd482b40c1fb5dd02bc38dd95cfbdd73c34629d2
Muhammad Faris Bin Shaik Mohamed <faris021120@gmail.com>
2025-04-18 17:04:47 +0800

    latestpush

---
+++ num1=5000 copy.py
@@ -0,0 +1,48 @@
+# ps5=5000
+# budget = int(input ("Enter you budget "))
+
+# if budget >= 5000 :
+#     num_5= budget // ps5
+#     print("you can buy",num_5,"ps5")
+#     balance = budget % ps5
+#     print("you balance", balance)
+    
+# else:
+#     print("You cant buy ps5" )
+
+# fruits = ["test2","test1","test3"]
+
+# print(fruits)
+# new = (input("enter the name of fruits "))
+# fruits.append(new)
+# fruits.pop(1)
+
+# for fruit in fruits :
+#     print(len(fruit))
+
+# correct_num =7
+# guess = 0
+
+# while guess != correct_num  :
+#     guess = int(input("guess the correct number "))
+#     if guess == correct_num :
+#         print("correct")
+#     else:
+#         print("try again")
+# num = 10  
+
+# while num > 2:  # Corrected syntax (added colon)
+#     for num in range(2, 10):  # Corrected syntax (added colon)
+#         if num % 2 != 0:  # Corrected syntax (added colon)
+#             print(num)  # Print odd numbers
+#     break  # Breaks the while loop after one iteration
+
+num = 10 
+
+while num > 2 :
+    for num in range (2,10):
+        if num % 2 == 0:
+            print(num)
+        else:
+            ("error")
+    break
